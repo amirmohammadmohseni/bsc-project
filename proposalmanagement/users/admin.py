6ab19318f9ba37042bd68f712a263c9bad74f15c
AmirMohammadMohseni <mohseni.amir1@gmail.com>
2020-08-17 12:59:11 +0430

    Proposal App Models Added To Admin.

--- proposalmanagement/users/admin.py
+++ proposalmanagement/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(BaseUser)
+admin.site.register(Professor)
+admin.site.register(Student)
+admin.site.register(Group)
+admin.site.register(Field)
